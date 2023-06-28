e0e4b0f0011edc023a36f901fbe4dbfc651e3d75
Povilas <povilaskazlauskas2003@gmail.com>
2023-06-28 22:33:20 +0200

    navigation baras su logo aciu dievui pagaliau

--- components/Navbar/navbar.jsx
+++ components/Navbar/navbar.jsx
@@ -1,13 +1,12 @@
 import Link from 'next/link';
 import './navbarcss.css';
-import Image from 'next/image'
 import React from 'react';
 export function Navbar () {
     return(
         <nav className='flex justify-center items-center rectangle'>
         
-          <div className=''>
-                <Image unoptimized src="/logo2.png" width={210} height={210} alt='Logo picture'/>
+          <div className='w-52'>
+                <img src='/logo2.png'/>
           </div>
 
         </nav>
