92b45a0c8b65150fac9bbbc62abde0d564a7a99f
Povilas <povilaskazlauskas2003@gmail.com>
2023-06-29 13:55:06 +0200

    UI improvements

---
+++ .firebase/verslo-projektas-2/hosting/_next/static/chunks/e218cc02-1855f2b47e5bc054.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[625],{4821:function(t,n,a){a.d(n,{sOJ:function(){return r}});var e=a(3270);function r(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"}}]})(t)}}}]);
--- components/start.jsx
+++ components/start.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import styles from './start.css'
+import {MdNavigateNext} from 'react-icons/md';
 import {initialCompletion, nextCompletion} from '../functions/openai';
 import { useState } from "react";
 
@@ -10,35 +11,36 @@ export function Start(props) {
         setState(state-1);
     }
 return(
-<div className="flex flex-col space-y-16 items-center py-32">
+<div className="flex flex-col space-y-10 h-screen items-center justify-center py-28 bg-back">
     
-    <h1 className="w-4/5 text-4xl text-">Enter The Details For Your Interview</h1>
+    <h1 className="w-4/5 text-8xl font-medium">Enter The Details For Your</h1>
+    <h1 className = "w-4/5 text-8xl font-bold"> Interview</h1>
     
-<div className="flex w-4/5 space-x-10 items-center">
-    <div  className="h-48">
-    <label class="block text-black text-2xl mb-2" for="companyName">Company Name</label>
-    <textarea className=" block drop-shadow-xl w-64 h-36 py-2 px-2 rounded-md text-xl" id = "company" placeholder="Enter your text here..."></textarea>
+<div className="flex pt-4 w-4/5 space-x-10 items-center">
+    <div  className="h-56">
+    <label class="block text-black text-4xl mb-2" for="companyName">Company Name</label>
+    <textarea className=" block drop-shadow-xl w-96 h-48 py-2 px-2 rounded-md text-xl" id = "company" placeholder="Enter your text here..."></textarea>
     </div>
     <div>
-    <label class="block text-black  text-2xl mb-2" for="position">Position</label>
-    <textarea className=" drop-shadow-xl w-64 h-36 py-2 px-2 rounded-md text-xl" id = "position" placeholder="Enter your text here..."></textarea>
+    <label class="block text-black  text-4xl pt-5 mb-2" for="position">Position</label>
+    <textarea className=" drop-shadow-xl w-96 h-48 py-2 px-2 rounded-md text-xl" id = "position" placeholder="Enter your text here..."></textarea>
     </div>
     
-    <div className="grid-cols-1 w-48 space-y-6">
-    <select  className=" drop-shadow-xl h-1/3 w-48 py-2 px-2 rounded-md align-middle" id="typeQuestions">
+    <div className="grid-cols-1 w-64 h-52 space-y-6 text-2xl">
+    <select  className=" drop-shadow-xl h-1/3 w-64 py-2 px-2 rounded-md align-middle" id="typeQuestions">
           <option  value=""  selected disabled hidden>Type of Questions</option>
           <option value="HR">HR</option>
           <option value="Job Tasks">Job Tasks</option>
           <option value="HR and Job Tasks">Both</option>
     </select>
-    <select  className=" drop-shadow-xl h-1/3 w-48 py-2 px-2 rounded-md align-middle" onChange={e=>setState(e.target.value)} id="amount" >
+    <select  className=" drop-shadow-xl h-1/3 w-64 py-2 px-2 rounded-md align-middle" onChange={e=>setState(e.target.value)} id="amount" >
           <option  value="0"  selected disabled hidden>Interview Length</option>
           <option value="5" >Short (5 Questions)</option>
           <option value="8" >Medium (8 Questions)</option>
           <option value="12" >Long (12 Questions)</option>
     </select>
 
-    <button onClick = {() => {decrement(); initialCompletion();}} className="bg-blue-950 text-white drop-shadow-xl w-48 h-1/3 py-2 px-2 rounded-md align-middle">Start</button>
+    <button onClick = {() => {decrement(); initialCompletion();}} className="flex items-center bg-blue-950 font-medium text-4xl justify-end text-white drop-shadow-xl w-64 h-1/3 py-2 px-2 rounded-md align-middle hover:bg-black hover:text-5xl  duration-500 ">Start<div className="ml-5 "><MdNavigateNext size="70px"className="text-white"/></div></button>
     </div>
 
 
